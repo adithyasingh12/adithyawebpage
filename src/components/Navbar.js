4daad92924ab4e8f8525e2407173830865752948
adithyasingh12 <adithya_singh123@hotmail.com>
2024-09-13 14:33:49 -0400

    krushil test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href="#home">Adithya</a>
+        <a href="#home">Krushil</a>
       </div>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
